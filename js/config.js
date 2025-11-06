51187c9fa7574da38cdfa24e5122a43fa865f173
yavechnomolodoy <yavechnomolodoy@gmail.com>
2025-11-07 01:35:52 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,18 @@
+// Конфигурация сайта
+const CONFIG = {
+    SITE: {
+        NAME: 'Остров-убежище',
+        DESCRIPTION: 'Посреди океана проблем',
+        AUTHOR: 'yavechnomolodoy@gmail.com'
+    },
+    FEATURES: {
+        COORDINATES_TRACKING: true,
+        EMAIL_COPY: true,
+        SMOOTH_SCROLL: true
+    },
+    TIMING: {
+        NOTIFICATION_DURATION: 4000,
+        EMAIL_NOTIFICATION_DURATION: 2000,
+        DOUBLE_CLICK_DELAY: 500
+    }
+};
