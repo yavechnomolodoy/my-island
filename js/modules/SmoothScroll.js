825ca32b0ec4157d7f7b43255aee28d5e1596eab
yavechnomolodoy <yavechnomolodoy@gmail.com>
2025-11-07 01:39:50 +0300

    Create SmoothScroll.js

---
+++ js/modules/SmoothScroll.js
@@ -0,0 +1,26 @@
+class SmoothScroll {
+    constructor() {
+        this.init();
+    }
+    
+    init() {
+        const links = document.querySelectorAll('a[href^="#"]');
+        
+        links.forEach(link => {
+            link.addEventListener('click', function(e) {
+                e.preventDefault();
+                
+                const targetId = this.getAttribute('href');
+                if (targetId === '#') return;
+                
+                const targetElement = document.querySelector(targetId);
+                if (targetElement) {
+                    targetElement.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start'
+                    });
+                }
+            });
+        });
+    }
+}
